bdae22f760f6b124ea0c69248249329a6e60cdfa
bswiley <bswiley@gmail.com>
2023-07-23 13:16:24 -0500

    Initial commit2

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+      Here is where I go on and on about myself, but since I am not quoting Cicero's De finibus bonorum et malorum, my text at this point won't last very long. Not nearly as long as I would like it too. Certainly, not nearly as long as it probably should. Although, I should mention that the images below represent some of the projects "I have been working on" recently. Starting with the first project which is a really big deal and moving on down to my fifth project which probably won't have as much impact (since originally writing this, I did work on "project1".). Naturally, the current images don't themselves have any content because, at this stage all the projects are imaginary and my imagination is rather limited.
+      </p>
+    </div>
+  );
+}
