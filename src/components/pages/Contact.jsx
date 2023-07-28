f2f4bc26f719b4539f60b23904afd54b532d7afa
bswiley <bswiley@gmail.com>
2023-07-27 23:36:19 -0500

    preparing to deploy

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [isNameValid, setIsNameValid] = useState(true);
   const [isEmailValid, setIsEmailValid] = useState(true);
   const [isMessageValid, setIsMessageValid] = useState(true);
   const [isMessageSent, setIsMessageSent] = useState(false);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -21,6 +27,10 @@ const Contact = () => {
     // Email validation regex
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+    // Check if the name is not empty
+    const isValidName = name.trim() !== "";
+    setIsNameValid(isValidName);
+
     // Check if the email address is valid
     const isValidEmail = emailRegex.test(email);
     setIsEmailValid(isValidEmail);
@@ -29,10 +39,11 @@ const Contact = () => {
     const isValidMessage = message.trim() !== "";
     setIsMessageValid(isValidMessage);
 
-    if (isValidEmail && isValidMessage) {
+    if (isValidName && isValidEmail && isValidMessage) {
       // Display confirmation message and ask for confirmation to send the email
-      if (window.confirm(`From: ${email}\n\ntext: ${message}\n\nSend this email?`)) {
+      if (window.confirm(`From: ${email}\nName: ${name}\n\nMessage: ${message}\n\nSend this email?`)) {
         // If the user clicks "Yes", clear the inputs and show "message sent" alert
+        setName("");
         setEmail("");
         setMessage("");
         setIsMessageSent(true);
@@ -42,39 +53,49 @@ const Contact = () => {
   };
 
   return (
-    
-      <div>
+    <div>
       <h1>Contact Me</h1>
       <div className="contactForm">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          {!isEmailValid && <span className="error">Please enter a valid email address.</span>}
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            rows="15"
-            value={message}
-            onChange={handleMessageChange}
-          />
-          {!isMessageValid && <span className="error">Please enter a message.</span>}
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-      {isMessageSent && (
-        <div>
-          <p>Message sent.</p>
-        </div>
-      )}
-    </div></div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={handleNameChange}
+            />
+            {!isNameValid && <span className="error">Please enter your name.</span>}
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            {!isEmailValid && <span className="error">Please enter a valid email address.</span>}
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              rows="15"
+              value={message}
+              onChange={handleMessageChange}
+            />
+            {!isMessageValid && <span className="error">Please enter a message.</span>}
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+        {isMessageSent && (
+          <div>
+            <p>Message sent.</p>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
